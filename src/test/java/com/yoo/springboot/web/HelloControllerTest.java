0f921f7acb05a2e8bac420b882723b392ccfe7a7
jiwon <jiwonee22@naver.com>
2021-10-31 22:56:04 +0900

    dto test

--- src/test/java/com/yoo/springboot/web/HelloControllerTest.java
+++ src/test/java/com/yoo/springboot/web/HelloControllerTest.java
@@ -1,15 +1,17 @@
-package com.yoo.springboot;
+package com.yoo.springboot.web;
 
+import com.yoo.springboot.web.HelloController;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.hamcrest.Matchers;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -24,10 +26,18 @@ public class HelloControllerTest {
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
 
-
-
     }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", Matchers.is(name)))
+                .andExpect(jsonPath("$.amount", Matchers.is(amount)));
+    }
 
 
 }
